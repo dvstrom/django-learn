d745d95215875ddf21dfd5f6b3d4636b17da5794
dvstrom <dvstrom@gmail.com>
2017-03-16 15:19:24 +0800

    test about

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Rango says hey there world! <br/><a href='/rango/about'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page")
+    return HttpResponse("Rango says here is the about page <br/><a href='/rango/'>Index</a>")
